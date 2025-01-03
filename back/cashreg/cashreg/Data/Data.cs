22214bdac02b525d4ce26a7fd69abf5ea8f78478
5 cm/s <dogecool2004@gmail.com>
2025-01-03 11:33:31 +0100

    f

---
+++ back/cashreg/cashreg/Data/Data.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cashreg.Models;
+
+namespace cashreg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
---
+++ back/cashreg/cashreg/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cashreg.Models
+{
+ 
+        [Table("xxx")]
+        public class Product
+    {
+         
+        }
+    
+}
